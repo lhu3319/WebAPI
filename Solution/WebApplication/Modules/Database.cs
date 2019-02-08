81dc94fe37fa529ea3e3d957a8242817863c0253
Han <lhu3319@nate.com>
2019-02-08 13:53:51 +0900

    우분ㅁ투 접속

--- Solution/WebApplication/Modules/Database.cs
+++ Solution/WebApplication/Modules/Database.cs
@@ -24,7 +24,7 @@ namespace WebApplication.Modules
         {
             conn = new MySqlConnection();
             conn.ConnectionString
-                = string.Format("server={0};user={1};password={2};database={3};port={4};", "gdc3.gudi.kr", "root", "1234", "test", "26002");
+                = string.Format("server={0};user={1};password={2};database={3};port={4};", "192.168.3.151", "root", "1234", "test", "3306");
             try
             {
                 conn.Open();
